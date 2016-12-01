a3e7b0edacef7528d85ff96dbe0265c21c00e4c6
jeanduong <jean.duong@gmail.com>
2016-12-01 17:28:48 +0100

    Nothing

--- app/src/main/java/com/example/jduong/contactapplication/MainActivity.java
+++ app/src/main/java/com/example/jduong/contactapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.jduong.contactapplication;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
