e46fc6d8824559e7432efca53fafa897d426bcf1
jeanduong <jean.duong@gmail.com>
2016-12-01 18:40:54 +0100

    Fuck

--- app/src/main/java/com/example/jduong/contactapplication/MainActivity.java
+++ app/src/main/java/com/example/jduong/contactapplication/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.jduong.contactapplication;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
 
 }
